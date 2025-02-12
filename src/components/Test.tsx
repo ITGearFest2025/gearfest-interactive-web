688c2be8e8d12eb7b46a1a687b75fc01337f16b7
Chulinuwu <ananzxzoxd@gmail.com>
2025-02-12 23:35:51 +0700

    feat:continue button

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ContinueButton from "./ContinueButton/ContinueButton";
 
 function Test() {
-  return <div className=" bg-red-500 text-2xl flex">hello</div>;
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
+  useEffect(() => {
+    console.log("Clicked", clicked);
+  }, [clicked]);
+
+  return (
+    <main>
+      <div className="bg-red-500 text-2xl flex">hello</div>
+      <hr></hr>
+      <section>
+      <h3>Tap to continue button</h3>
+      <div className="bg-gray w-full h-[400px]">
+        <ContinueButton word="Tap to Continue" onClick={handleClick} />
+        {clicked && <p>Clicked</p>}
+      </div>
+      <hr></hr>
+      </section>
+    </main>
+  );
 }
 
 export default Test;
