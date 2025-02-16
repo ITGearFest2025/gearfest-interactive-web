22858bebb935c4275cace10babc6e09f5b15e800
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2025-02-16 22:26:18 +0700

    feat: add type "Box" for TextBox

--- src/components/StoryText/StoryText.tsx
+++ src/components/StoryText/StoryText.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 type TextBoxProps = {
-  type: "top" | "bottom" | "center";
+  type: "top" | "bottom" | "center" | "box";
   children?: React.ReactNode;
   word?: string;
   className?: string;
@@ -20,6 +20,7 @@ const TextBox: React.FC<TextBoxProps> = ({
     bottom:
       "bottom-0 h-[20%] bg-black/30 w-full flex items-center justify-center",
     center: "top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2",
+    box: "relative",
   };
 
   return (
@@ -27,17 +28,6 @@ const TextBox: React.FC<TextBoxProps> = ({
       {children ? children : <p className="text-xl">{word}</p>}
     </div>
   );
-  // return (
-  //   <div className={`${baseClasses} ${typeClasses[type]} ${className}`}>
-  //     {type === "bottom" ? (
-  //       <div className="absolute right-0 bottom-0 left-0 flex h-[30%] flex-col justify-start bg-black p-5 text-white opacity-50">
-  //         {children}
-  //       </div>
-  //     ) : (
-  //       <div>{children}</div>
-  //     )}
-  //   </div>
-  // );
 };
 
 export default TextBox;
