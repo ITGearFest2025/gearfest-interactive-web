7f0d70e068a009056bffd565da5866281aba3acb
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2025-02-16 16:34:26 +0700

    style: change pulse style

--- src/components/ContinueButton/ContinueButton.tsx
+++ src/components/ContinueButton/ContinueButton.tsx
@@ -1,27 +1,36 @@
-import type React from "react"
+import type React from "react";
 
 interface ContinueButtonProps {
-  word: string
-  onClick: () => void
-  position: "top" | "bottom" | "center"
-  className?: string
+  word: string;
+  onClick: () => void;
+  position: "top" | "bottom" | "center";
+  className?: string;
 }
 
-const ContinueButton: React.FC<ContinueButtonProps> = ({ word, onClick, position, className = "" }) => {
-  const baseClasses = "z-50 transition-all hover:scale-105 bg-transparent cursor-pointer"
+const ContinueButton: React.FC<ContinueButtonProps> = ({
+  word,
+  onClick,
+  position,
+  className = "",
+}) => {
+  const baseClasses = "z-50 animate-pulse bg-transparent cursor-pointer";
 
   const positionClasses = {
     top: "absolute left-4 top-[30%]",
     bottom: "absolute right-4 bottom-4",
     center: "absolute left-1/2 bottom-[30%] -translate-x-1/2",
-  }
+  };
 
   return (
-    <div className={`${baseClasses} ${positionClasses[position]} ${className}`} onClick={onClick}>
-      <p className="px-3 py-2 bg-transparent bg-opacity-50 text-black rounded">{word}</p>
+    <div
+      className={`${baseClasses} ${positionClasses[position]} ${className}`}
+      onClick={onClick}
+    >
+      <p className="bg-opacity-50 rounded bg-transparent px-3 py-2 text-black">
+        {word}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default ContinueButton
-
+export default ContinueButton;
