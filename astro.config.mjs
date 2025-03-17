428a0fe181b463d06fca9c07fbe98a38179478aa
chulinuwuw <ananzxzoxd@gmail.com>
2025-03-17 11:23:03 +0700

    init navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -11,5 +12,5 @@ export default defineConfig({
     svg: true,
   },
 
-  integrations: [react()],
+  integrations: [react(), svelte()],
 });
