a0a50ec261037a5742c74bbf84d539ec23425149
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2025-03-19 02:56:19 +0700

    feat: add jigsaw scene

---
+++ src/components/jigsaw/Draggable.tsx
@@ -0,0 +1,32 @@
+import { useDraggable } from "@dnd-kit/core";
+
+interface DraggableProps {
+  id: string;
+  className?: string;
+  children: React.ReactNode;
+}
+
+export function Draggable({ id, className, children }: DraggableProps) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id,
+  });
+
+  const style = {
+    transform: transform
+      ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
+      : "",
+    touchAction: "none",
+  };
+
+  return (
+    <button
+      className={className}
+      ref={setNodeRef}
+      style={style}
+      {...listeners}
+      {...attributes}
+    >
+      {children}
+    </button>
+  );
+}
