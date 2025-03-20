c62e2a76629b6b7935a3b555e3320640a2f5ff01
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2025-03-21 06:35:12 +0700

    style: update style in landing page

--- src/components/Landing/LandingPage.tsx
+++ src/components/Landing/LandingPage.tsx
@@ -42,57 +42,7 @@ const LandingPage = () => {
           "linear-gradient(180deg, rgba(0, 0, 0, 0) 64.4%, rgba(226, 199, 232, 0.18) 91%, rgba(239, 182, 200, 0.2) 96%)",
       }}
     >
-      {result === "" ? (
-        <>
-          <div className="mt-16 flex w-auto items-center justify-center gap-1.5">
-            <img src={ESC.src} width={53} height={69} alt="ESC" />
-            <img src={GearFest.src} width={136} height={136} alt="GearFest" />
-            <img src={KNEng.src} width={53} height={53} alt="KNEng" />
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <img
-              src={Logo.src}
-              alt="from Scars to Star logo"
-              className="h-full min-h-[100px] w-full max-w-[404px] min-w-[200px] px-6"
-            />
-            <a
-              href="/story/scene0/0-1"
-              style={{
-                boxShadow: "0px 0px 10px 2px #E2C7E8",
-                textShadow: "0px 0px 10px #E2C7E8",
-              }}
-              className="font-italiana mt-10 rounded-full border-[2px] border-white bg-transparent px-12 py-3.5 text-2xl text-white transition-all duration-300 hover:scale-110 active:scale-105"
-            >
-              Start Journey
-            </a>
-          </div>
-          <div className="font-judson flex flex-col items-center justify-center gap-2.5">
-            <a
-              href="https://www.instagram.com/gearfestival.official/"
-              className="text-judson flex gap-1.5 text-white"
-            >
-              <img src={Instagram.src} width={25} height={25} alt="Instagram" />
-              gearfestival.official
-            </a>
-            <a
-              href="https://www.tiktok.com/@gearfestival_official"
-              className="text-judson flex gap-1.5 text-white"
-            >
-              <img src={Tiktok.src} width={24} height={27.5} alt="Tiktok" />
-              gearfestival.official
-            </a>
-          </div>
-          <div className="mb-4 flex flex-col items-center">
-            <p
-              style={{ textShadow: "0px 0px 4px #FFFFFF" }}
-              className="font-judson text-[#F7DEFC]"
-            >
-              What is Gear Festival?
-            </p>
-            <img src={ArrowDown.src} width={36} height={36} alt="ArrowDown" />
-          </div>
-        </>
-      ) : (
+      {result !== "" ? (
         <>
           <div className="mt-6 flex w-auto items-center justify-center gap-1.5">
             <img src={ESC.src} width={53} height={69} alt="ESC" />
@@ -143,6 +93,56 @@ const LandingPage = () => {
             <img src={Line.src} width={164} height={1} alt="line" />
           </div>
           <div className="font-judson mt-6 flex flex-col items-center justify-center gap-2.5">
+            <a
+              href="https://www.instagram.com/gearfestival.official/"
+              className="text-judson flex gap-1.5 text-white"
+            >
+              <img src={Instagram.src} width={25} height={25} alt="Instagram" />
+              gearfestival.official
+            </a>
+            <a
+              href="https://www.tiktok.com/@gearfestival_official"
+              className="text-judson flex gap-1.5 text-white"
+            >
+              <img src={Tiktok.src} width={24} height={27.5} alt="Tiktok" />
+              gearfestival.official
+            </a>
+          </div>
+          <div className="mb-4 flex flex-col items-center">
+            <p
+              style={{ textShadow: "0px 0px 4px #FFFFFF" }}
+              className="font-judson text-[#F7DEFC]"
+            >
+              What is Gear Festival?
+            </p>
+            <img src={ArrowDown.src} width={36} height={36} alt="ArrowDown" />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="mt-16 flex w-auto items-center justify-center gap-1.5">
+            <img src={ESC.src} width={53} height={69} alt="ESC" />
+            <img src={GearFest.src} width={136} height={136} alt="GearFest" />
+            <img src={KNEng.src} width={53} height={53} alt="KNEng" />
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <img
+              src={Logo.src}
+              alt="from Scars to Star logo"
+              className="h-full min-h-[100px] w-full max-w-[404px] min-w-[200px] px-6"
+            />
+            <a
+              href="/story/scene0/0-1"
+              style={{
+                boxShadow: "0px 0px 10px 2px #E2C7E8",
+                textShadow: "0px 0px 10px #E2C7E8",
+              }}
+              className="font-italiana mt-10 rounded-full border-[2px] border-white bg-transparent px-12 py-3.5 text-2xl text-white transition-all duration-300 hover:scale-110 active:scale-105"
+            >
+              Start Journey
+            </a>
+          </div>
+          <div className="font-judson flex flex-col items-center justify-center gap-2.5">
             <a
               href="https://www.instagram.com/gearfestival.official/"
               className="text-judson flex gap-1.5 text-white"
