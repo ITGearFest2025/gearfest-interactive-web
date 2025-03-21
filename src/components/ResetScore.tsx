380b9a2e21275daff49ad9899b52563bb9dcd79b
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2025-03-21 16:20:39 +0700

    feat: add reset long answer

--- src/components/ResetScore.tsx
+++ src/components/ResetScore.tsx
@@ -1,8 +1,12 @@
 import { userAnswer } from "@/stores/userAnswer";
+import { userLongAnswer } from "@/stores/userLongAnswer";
 const ResetScore = () => {
   const scores = [1, 2, 3, 4, 5, 6, 7, 8, 9].map((item) => {
     const typeName = "type" + item.toString();
     userAnswer.setKey(typeName, "0");
+  });
+  const longAnswer = ["userAnswer", "tellUs", "wish"].map((item) => {
+    userLongAnswer.setKey(item, "");
   });
 };
 
