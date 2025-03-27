44c5d67bb228e99bef91c99c37bfa438113e499e
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2025-03-28 04:51:09 +0700

    refactor: re folder component name from KeepButton to Onsite

--- src/components/Onsite/ScanMoreItem.tsx
+++ src/components/Onsite/ScanMoreItem.tsx
@@ -5,6 +5,9 @@ export default function ColorBox() {
 
   return (
     <div
+      style={{
+        boxShadow: "0px 0px 10px 2px #E2C7E8",
+      }}
       className="inline-flex h-[47px] w-[167px] cursor-pointer items-center justify-center rounded-full border-2 border-white px-4 py-2 text-white transition duration-300"
       onClick={handleClick}
     >
